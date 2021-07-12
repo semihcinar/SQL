f1a3ee75fa5b3cbd23b4ec23c1e1d216dc1475b5
Semih ÇINAR <82849253+semihcinar@users.noreply.github.com>
2021-07-12 17:32:40 +0300

    Create StunOrtalama.sql

---
+++ StunOrtalama.sql
@@ -0,0 +1,16 @@
+select year(Tarih), month(Tarih), simdi, dovizturu from DovizDepo 
+where year(tarih)='2021'  
+
+
+
+
+-- BU SORGUDA 
+-- Tablomuzda USD - AVRO - TRY DEÐÝÞÝMLERÝ YERALMAKTADIR.
+-- YIL VE AY BAZINDA avro - try DEÐÝÞÝMLERÝNÝN AYLIK ORTALAMALARI HESAPLNAMIÞTIR.
+
+-- TABLOMUZDA 
+-- [Tarih], DATETÝME
+-- [simdi], FLOAT
+-- [dovizturu] CHAR
+
+-- ALANLARI YERALMAKTADIR.
