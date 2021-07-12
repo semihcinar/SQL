afdd03f2c48f989e1a9c76fcab44cf2554ab2702
Semih ÇINAR <82849253+semihcinar@users.noreply.github.com>
2021-07-13 02:46:16 +0300

    Create Update1.sql

---
+++ Update1.sql
@@ -0,0 +1,4 @@
+--TABLODA YER ALAN BÝR STUN ÝÇÝNE AYNI BÝLGÝYÝ YAZMAK ÝÇÝN update KOMUTUNU KUYLLANIYORUZ. TABLO ADI (houses_ist2) STUN ADI (il) ÝL ALANINA GÝRÝLEN VERÝ (ÝSTANBUL) 
+
+UPDATE houses_ist2 
+SET il ='istanbul';
