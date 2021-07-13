9cf29c5d87e85767d4464b144b4886e9ff546689
Semih ÇINAR <82849253+semihcinar@users.noreply.github.com>
2021-07-14 02:10:34 +0300

    Create DovizDepoOrtalamaDovizturu.sql

---
+++ DovizDepoOrtalamaDovizturu.sql
@@ -0,0 +1,15 @@
+SELECT        AVG(simdi) AS FIYAT, dovizturu, YEAR(Tarih) AS YIL, MONTH(Tarih) AS AY
+FROM            DovizDepo
+GROUP BY YEAR(Tarih), MONTH(Tarih), dovizturu
+ORDER BY YIL DESC, AY
+
+
+
+--1990 yýlýndan itibaren bulunan doviz fiyatlarý aylýk ve týllýk olarak ortalamalrý hesaplanmýþtýr.
+--Tarih Datetime
+--simdi float anlýk döviz fiyatýný verir
+--Döviz türü char Dövizin türünü ifade eder
+
+--Görselleþtirmelere ve data setine kaggle.com dan ulaþabilirsiniz.
+-- hesap: https://www.kaggle.com/semihnar
+-- Githup :https://github.com/semihcinar
