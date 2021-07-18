ac86386c091460d85ab29e3a24a4a7e0df433113
Semih ÇINAR <82849253+semihcinar@users.noreply.github.com>
2021-07-18 03:42:57 +0300

    Create 0_NotExist.sql

---
+++ 0_NotExist.sql
@@ -0,0 +1,11 @@
+SELECT	DISTINCT STATE
+FROM	sales.customers X
+WHERE	 NOT EXISTS 		(
+							SELECT	1
+							FROM	production.products A, sales.order_items B, sales.orders C, sales.customers D
+							WHERE	A.product_id = B.product_id
+							AND		B.order_id = C.order_id
+							AND		C.customer_id = D.customer_id
+							AND		A.product_name = 'Trek Remedy 9.8 - 2017'
+							AND		X.state = D.state
+							)
