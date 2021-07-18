3006e71bef362327e0b606929c566d8bf9bb41af
Semih ÇINAR <82849253+semihcinar@users.noreply.github.com>
2021-07-18 03:42:54 +0300

    Create 0_Create_Wiev.sql

---
+++ 0_Create_Wiev.sql
@@ -0,0 +1,12 @@
+CREATE VIEW SUMMARY_VIW AS
+SELECT	first_name, last_name, order_date, product_name, model_year,
+		quantity, list_price, final_price
+FROM
+		(
+		SELECT	A.first_name, A.last_name, B.order_date, D.product_name, D.model_year,
+				C.quantity, C.list_price, C.list_price * (1-C.discount) final_price
+		FROM	sales.customers A, sales.orders B, sales.order_items C, production.products D
+		WHERE	A.customer_id = B.customer_id AND
+				B.order_id = C.order_id AND
+				C.product_id = D.product_id
+		) A
