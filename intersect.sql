9ccb289eb850e7e7d71b267c2749ad1058fb9169
Semih ÇINAR <82849253+semihcinar@users.noreply.github.com>
2021-07-31 11:48:15 +0300

    Create intersect.sql

---
+++ intersect.sql
@@ -0,0 +1,33 @@
+-- INTERSECT --
+
+SELECT	A.customer_id, A.first_name, A.last_name
+FROM	sales.customers A, sales.orders B
+WHERE	A.customer_id = B.customer_id
+AND		B.order_id IN (
+					SELECT	B.order_id
+					FROM	production.products A, sales.order_items B
+					WHERE	A.product_id = B.product_id
+					AND		A.category_id = (
+												SELECT	category_id
+												FROM	production.categories
+												WHERE	category_name = 'Electric Bikes'
+											)
+					INTERSECT
+					SELECT	B.order_id
+					FROM	production.products A, sales.order_items B
+					WHERE	A.product_id = B.product_id
+					AND		A.category_id = (
+												SELECT	category_id
+												FROM	production.categories
+												WHERE	category_name = 'Comfort Bicycles'
+											)
+					INTERSECT
+					SELECT	B.order_id
+					FROM	production.products A, sales.order_items B
+					WHERE	A.product_id = B.product_id
+					AND		A.category_id = (
+												SELECT	category_id
+												FROM	production.categories
+												WHERE	category_name = 'Children Bicycles'
+											)
+					)
