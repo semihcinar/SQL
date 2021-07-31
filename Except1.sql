7387a82d856d1114a92bf5fd6a301b072936076c
Semih ÇINAR <82849253+semihcinar@users.noreply.github.com>
2021-08-01 00:23:56 +0300

    Create Except1.sql

---
+++ Except1.sql
@@ -0,0 +1,21 @@
+-- EXCEPT 
+
+-- iki farklý kümedeki birinde olup diðerinde olmayan kayýtlarý listeler. 
+
+-- Write a query that returns only products ordered in 2017 (not ordered in other years).
+
+SELECT	product_id, product_name
+FROM	production.products
+WHERE	product_id IN (
+					SELECT	DISTINCT B.product_id
+					FROM	sales.orders A, sales.order_items B
+					WHERE	A.order_id= B.order_id
+					AND		A.order_date BETWEEN '2017-01-01' AND '2017-12-31'
+
+					EXCEPT
+
+					SELECT	DISTINCT B.product_id
+					FROM	sales.orders A, sales.order_items B
+					WHERE	A.order_id= B.order_id
+					AND		A.order_date NOT BETWEEN '2017-01-01' AND '2017-12-31'
+					)
