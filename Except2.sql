d1436a0f14ce99176e8d7c8ee7cb43adc5aa60eb
Semih ÇINAR <82849253+semihcinar@users.noreply.github.com>
2021-08-01 00:24:00 +0300

    Create Except2.sql

---
+++ Except2.sql
@@ -0,0 +1,17 @@
+-- Except 2
+
+
+
+
+SELECT STATE
+FROM	sales.customers
+
+EXCEPT
+
+SELECT	D.STATE
+FROM	production.products A, sales.order_items B, sales.orders C, sales.customers D
+WHERE	A.product_id = B.product_id
+AND		B.order_id = C.order_id
+AND		C.customer_id = D.customer_id
+AND		A.product_name = 'Trek Remedy 9.8 - 2017'
+
