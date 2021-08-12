9df11cb332c4ccb937ae03f1263bd4544c0a2c23
Semih ÇINAR <82849253+semihcinar@users.noreply.github.com>
2021-08-13 02:36:41 +0300

    Create Trim.sql

---
+++ Trim.sql
@@ -0,0 +1,17 @@
+--TRIM
+
+SELECT TRIM(' CHARACTER ')
+
+SELECT TRIM (' CHARACT ER')
+
+SELECT TRIM('./' FROM '/character..') result
+
+---
+
+--LTRIM
+
+SELECT LTRIM(' CHARACTER ')
+
+--RTRIM
+
+SELECT RTRIM(' CHARACTER ')
