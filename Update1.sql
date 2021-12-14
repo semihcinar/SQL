caf7dca103e4c9e0d5b8f86c3dfbd147ee85362c
Semih ÇINAR <semihcinar@gmail.com>
2021-12-15 02:28:52 +0300

    Update Update1.sql

--- Update1.sql
+++ Update1.sql
@@ -1,7 +1,22 @@
 --TABLODA YER ALAN BÝR STUN ÝÇÝNE AYNI BÝLGÝYÝ YAZMAK ÝÇÝN update KOMUTUNU KUYLLANIYORUZ. TABLO ADI (houses_ist2) STUN ADI (il) ÝL ALANINA GÝRÝLEN VERÝ (ÝSTANBUL) 
 
-UPDATE houses_ist2 
-SET il ='istanbul';
+UPDATE Cleaned_segmentation_V4_Score
+SET Monetary_Score ='4'
+where MonetaryR > 1571.28;
+
+
+
+
+
+
+
+
+
+
+
+UPDATE product.product
+SET list_price ='59'
+where product_name like '%COSORI CO108%' AND product.brand_id=1 AND product.category_id=2;
 
 
 
