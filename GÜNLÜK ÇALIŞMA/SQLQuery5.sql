8758f24ebbbe010a83a80093b18df46320bddf96
Semih ÇINAR <semihcinar@gmail.com>
2021-12-17 03:56:36 +0300

    Create SQLQuery5.sql

---
+++ GÜNLÜK ÇALIŞMA/SQLQuery5.sql
@@ -0,0 +1,11 @@
+SELECT        TOP (100) customer_id, first_name, last_name, street, city, state, zip, phone, email
+FROM            customer
+WHERE        (customer_id = 901)
+
+SELECT        TOP (100) customer_id, first_name, last_name, street, city, state, zip, phone, email
+FROM            customer
+WHERE        (customer_id = 902)
+
+SELECT        TOP (100) customer_id, first_name, last_name, street, city, state, zip, phone, email
+FROM            customer
+WHERE        (customer_id = 903)
