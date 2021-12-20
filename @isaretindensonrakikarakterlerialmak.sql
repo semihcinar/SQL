e374bc14fc2f4d19187ff7d7f514e47420761e87
Semih ÇINAR <semihcinar@gmail.com>
2021-12-20 23:23:17 +0300

    Update @isaretindensonrakikarakterlerialmak.sql

--- @isaretindensonrakikarakterlerialmak.sql
+++ @isaretindensonrakikarakterlerialmak.sql
@@ -1,4 +1,9 @@
 select substring (email, CHARINDEX ('@', email)+1, LEN(email)) as eposta,   count (substring (email, CHARINDEX ('@', email)+1, LEN(email))) as toplam
-from sales.customers
+from customer
 group by substring (email, CHARINDEX ('@', email)+1, LEN(email))
 
+
+
+select substring (email, CHARINDEX ('.', email)+1, LEN(email)) as eposta
+from customer
+group by substring (email, CHARINDEX ('.', email)+1, LEN(email))
