d4685b3c4dc1d1461b5f97cb1133ab00485f9c78
Semih ÇINAR <semihcinar@gmail.com>
2021-12-24 02:23:18 +0300

    Update Update.sql

--- EGITIM/Update.sql
+++ EGITIM/Update.sql
@@ -7,3 +7,24 @@ UPDATE Query
 WHERE        ModelCodeOrj = '0588/AZD' AND GearType = 'Automatic'
 
 -----------------------------------------------------------------------------------------------------------
+
+
+							DECLARE @ID AS INT
+							DECLARE @I AS INT=0
+							WHILE @I < 200
+							BEGIN
+
+							SET @ID= ROUND(RAND()*1000,0);
+							
+							SET @I=@I+1
+							END
+
+							
+
+UPDATE customer
+	SET phone= NULL
+WHERE  customer_id IN(
+						SELECT @ID
+						)
+
+					
