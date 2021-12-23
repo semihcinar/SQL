c5ccca493097741a63783b6443192c82fcc5044f
Semih ÇINAR <semihcinar@gmail.com>
2021-12-23 04:18:13 +0300

    Create capstone3.sql

---
+++ GÜNLÜK ÇALIŞMA/capstone3.sql
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+Select  Time, V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15, V16, V17, V18, V19, V20, V21, V22, V23, V24, V25, V26, V27, V28, Amount, Class
+From creditcard
+Group By Time, V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15, V16, V17, V18, V19, V20, V21, V22, V23, V24, V25, V26, V27, V28, Amount, Class
+Having Count (Time) > 1
+
+
+select count(*) from creditcard
+
+
+
+
+DELETE FROM creditcard
+WHERE id NOT IN
+(
+	SELECT MAX(id)
+		From creditcard
+		Group By Time, V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15, V16, V17, V18, V19, V20, V21, V22, V23, V24, V25, V26, V27, V28, Amount, Class
+);
