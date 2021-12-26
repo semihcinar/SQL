b90d51b10a845c335f1ef0c1eb8539a74dfa1e46
Semih ÇINAR <semihcinar@gmail.com>
2021-12-27 01:54:11 +0300

    Create OlmayanListele.sql

---
+++ GÜNLÜK ÇALIŞMA/OlmayanListele.sql
@@ -0,0 +1,11 @@
+
+
+
+
+
+SELECT        SELECT ID
+FROM            Data
+WHERE        (brand <> N'1') AND (brand <> N'2') AND (brand <> N'3') AND (brand <> N'4') AND (brand <> N'5') AND (brand <> N'6') AND (brand <> N'7') AND (brand <> N'8') AND (brand <> N'9') AND (brand <> N'10') AND (brand <> N'11') AND 
+                         (brand <> N'12') AND (brand <> N'13') AND (brand <> N'14') AND (brand <> N'15') AND (brand <> N'16') AND (brand <> N'17') AND (brand <> N'18') AND (brand <> N'19') AND (brand <> N'20') AND (brand <> N'21') AND 
+                         (brand <> N'22') AND (brand <> N'23') AND (brand <> N'24') AND (brand <> N'25') AND (brand <> N'26') AND (brand <> N'27') AND (brand <> N'28') AND (brand <> N'29') AND (brand <> N'30') AND (brand <> N'31') AND 
+                         (brand <> N'32') AND (brand <> N'33') AND (brand <> N'34') AND (brand <> N'35') AND (brand <> N'36') AND (brand <> N'37') AND (brand <> N'38') AND (brand <> N'39') AND (brand <> N'40')
