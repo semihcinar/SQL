8935cb8308ae0d468829f04c25fd5b1d3a690c3a
Semih ÇINAR <semihcinar@gmail.com>
2022-01-13 02:43:04 +0300

    Create MailGonderHtml.sql

---
+++ EGITIM/MailGonderHtml.sql
@@ -0,0 +1,29 @@
+DECLARE @tableHTML  NVARCHAR(MAX) ;  
+  
+SET @tableHTML =  
+    N'<H1>Work Order Report</H1>' +  
+    N'<table border="1">' +  
+    N'<tr><th>Work Order ID</th><th>Product ID</th>' +  
+    N'<th>Name</th><th>Order Qty</th><th>Due Date</th>' +  
+    N'<th>Expected Revenue</th></tr>' +  
+    CAST ( ( SELECT td = wo.WorkOrderID,       '',  
+                    td = p.ProductID, '',  
+                    td = p.Name, '',  
+                    td = wo.OrderQty, '',  
+                    td = wo.DueDate, '',  
+                    td = (p.ListPrice - p.StandardCost) * wo.OrderQty  
+              FROM AdventureWorks.Production.WorkOrder as wo  
+              JOIN AdventureWorks.Production.Product AS p  
+              ON wo.ProductID = p.ProductID  
+              WHERE DueDate > '2004-04-30'  
+                AND DATEDIFF(dd, '2004-04-30', DueDate) < 2   
+              ORDER BY DueDate ASC,  
+                       (p.ListPrice - p.StandardCost) * wo.OrderQty DESC  
+              FOR XML PATH('tr'), TYPE   
+    ) AS NVARCHAR(MAX) ) +  
+    N'</table>' ;  
+  
+EXEC msdb.dbo.sp_send_dbmail @recipients='yourfriend@Adventure-Works.com',  
+    @subject = 'Work Order List',  
+    @body = @tableHTML,  
+    @body_format = 'HTML' ;  
