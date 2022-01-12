fc3615a2e649eff7d3b864c9ee6e025fd2d4db61
Semih ÇINAR <semihcinar@gmail.com>
2022-01-13 02:43:02 +0300

    Create reverse.sql

---
+++ EGITIM/reverse.sql
@@ -0,0 +1,16 @@
+select REVERSE(first_name) as fnameK,
+		REVERSE(first_name) as FnameB,	
+
+* from sale.customer
+
+
+-- Sayýlarý belli karaktere tamamlamak için kullanýlýr
+
+select REPLICATE ('0', 10)
+
+
+--REPLACE
+select replace ('deðiþecek alan', 'eski kelime', ' yenikelime')
+
+
+select REPLACE ('Semih ÇINAR', 'mih', 'leh')
