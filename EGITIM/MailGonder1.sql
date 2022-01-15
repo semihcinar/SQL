374570b8215196cc8564b3a35a5ff74b5d651973
Semih ÇINAR <semihcinar@gmail.com>
2022-01-15 13:27:44 +0300

    Update MailGonder1.sql

--- EGITIM/MailGonder1.sql
+++ EGITIM/MailGonder1.sql
@@ -1,5 +1,33 @@
+
+declare @eposta varchar (70)
+	set @eposta= (select eposta from eposta)
+
 EXEC msdb.dbo.sp_send_dbmail  
-    @profile_name = 'Semihcinarcomtr',  
-    @recipients = 'semihcinar@gmail.com',  
-    @body = 'kontrol',  
-    @subject = 'kontrol' ;  
+    @profile_name = 'Semihcinarcomtr',
+    @recipients = @eposta,
+    @subject = 'sql dersi baþlýyor',
+	@body = 'SQL otomatik eposta gönderimi, SQL dersi baþlýyor.';  
+
+
+
+
+
+
+
+
+declare @id INT
+Set @id = (Select id from eposta)
+
+DECLARE @I AS INT=0
+WHILE @I < 10
+BEGIN
+
+
+select eposta from eposta 
+
+
+SET @I=@I+1
+
+END
+
+select * from eposta
