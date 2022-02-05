6578e33ac203dc5e48793ee0b13c253dc592af7d
Semih ÇINAR <semihcinar@gmail.com>
2022-02-06 02:05:12 +0300

    Create Postagonder.sql

---
+++ GÜNLÜK ÇALIŞMA/Postagonder.sql
@@ -0,0 +1,23 @@
+
+EXEC msdb.dbo.sp_send_dbmail  
+    @profile_name = 'Semihcinarcomtr',  
+    @recipients = 'semihcinar@gmail.com',  
+
+
+
+ @query='SELECT        TOP (10) product.product.product_id, product.product.product_name, 
+ product.product.brand_id, product.product.category_id, product.product.model_year, product.product.list_price, 
+ product.stock.quantity FROM product.product INNER JOIN  
+ product.stock ON product.product.product_id = product.stock.product_id WHERE        (product.stock.quantity < 5)',
+ 
+ 
+ @subject = 'Work Order Count',  
+    @attach_query_result_as_file = 1 ; 
+
+
+	use SampleRetail
+	EXEC msdb.dbo.sp_send_dbmail  
+    @profile_name = 'ekspercin',  
+    @recipients = 'semihcinar@gmail.com',  
+    @query = 'SELECT  top (10)  * from product.product',  
+    @attach_query_result_as_file = 1 ;  
