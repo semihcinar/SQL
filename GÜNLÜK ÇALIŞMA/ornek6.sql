4d3326c587d535ee3af291a644621366f69976b7
Semih ÇINAR <semihcinar@gmail.com>
2022-02-25 02:38:25 +0300

    Create ornek6.sql

---
+++ GÜNLÜK ÇALIŞMA/ornek6.sql
@@ -0,0 +1,26 @@
+SELECT  product.product.product_id
+FROM     product.product INNER JOIN
+                  sale.order_item ON product.product.product_id = sale.order_item.product_id INNER JOIN
+                  sale.orders ON sale.order_item.order_id = sale.orders.order_id
+WHERE  (2019 = YEAR(sale.orders.order_date))
+
+
+EXCEPT
+
+
+SELECT  product.product.product_id
+FROM     product.product INNER JOIN
+                  sale.order_item ON product.product.product_id = sale.order_item.product_id INNER JOIN
+                  sale.orders ON sale.order_item.order_id = sale.orders.order_id
+WHERE  (2018 = YEAR(sale.orders.order_date))
+
+
+except
+
+
+
+SELECT  product.product.product_id
+FROM     product.product INNER JOIN
+                  sale.order_item ON product.product.product_id = sale.order_item.product_id INNER JOIN
+                  sale.orders ON sale.order_item.order_id = sale.orders.order_id
+WHERE  (2020 = YEAR(sale.orders.order_date))
