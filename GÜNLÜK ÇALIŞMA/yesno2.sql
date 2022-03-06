8bde23ee534dab8ba8394ad9a1e61e7973a0d457
Semih ÇINAR <semihcinar@gmail.com>
2022-03-07 01:16:39 +0300

    Create yesno2.sql

---
+++ GÜNLÜK ÇALIŞMA/yesno2.sql
@@ -0,0 +1,6 @@
+SELECT	distinct D.customer_id, D.first_name, D.last_name
+FROM	product.product A, sale.order_item B, sale.orders C, sale.customer D
+WHERE	A.product_id=B.product_id
+AND		B.order_id = C.order_id
+AND		C.customer_id = D.customer_id
+AND		A.product_name = '2TB Red 5400 rpm SATA III 3.5 Internal NAS HDD'
