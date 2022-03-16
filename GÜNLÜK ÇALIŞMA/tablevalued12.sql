9b31a28b85733a18d7887e4d26c91307838400d0
Semih ÇINAR <semihcinar@gmail.com>
2022-03-16 03:13:59 +0300

    Create tablevalued12.sql

---
+++ GÜNLÜK ÇALIŞMA/tablevalued12.sql
@@ -0,0 +1,14 @@
+alter function twitbul (@id varchar (50))
+RETURNS TABLE
+as
+RETURN
+select * from Tweet_import
+where [id]= @id
+
+where [id_str] 
+
+
+select * from twitbul (850125)
+select sp_spaceused Tweet_import
+
+
